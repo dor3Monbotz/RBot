60d283dded59505c761cd1beb9421e17c5ba39a4
Gaikwad Kunal <118621248+dor3Monbotz@users.noreply.github.com>
2023-10-18 09:22:20 +0530

    Update config.py

--- MeshRenameBot/config.py
+++ MeshRenameBot/config.py
@@ -4,10 +4,10 @@ try:
     from .tconfig import Config
 except ImportError:
     class Config:
-        DATABASE_URL = [str, ""]
-        API_HASH = [str, "abcdedf......"]
-        API_ID = [int, 1234567]
-        BOT_TOKEN = [str, "bot:token here"]
+        DATABASE_URL = [str, "mongodb+srv://Nano12:Nano12@cluster0.pz7kgyi.mongodb.net/?retryWrites=true&w=majority"]
+        API_HASH = [str, "04aeab3df8b9332d208837938c7ce29b"]
+        API_ID = [int, 20959453]
+        BOT_TOKEN = [str, "bot:6295293651:AAGyileBj3sDxxThMPpyIbq4qVRLbLyqP0Y"]
         COMPLETED_STR = [str, "▰"]
         REMAINING_STR = [str, "▱"]
         MAX_QUEUE_SIZE = [int, 5]
@@ -16,12 +16,12 @@ except ImportError:
 
         # Access Restriction
         IS_PRIVATE = [bool, False]
-        AUTH_USERS = [list,[123456789]]
-        OWNER_ID = [int, 0]
+        AUTH_USERS = [list,[6299128233]]
+        OWNER_ID = [int, 6265459491]
 
         # Public username url or invite link of private chat
         FORCEJOIN = [str,""]
-        FORCEJOIN_ID = [int,-100123465978]
+        FORCEJOIN_ID = [int,-1001912292553]
 
         TRACE_CHANNEL = [int, 0]
 
